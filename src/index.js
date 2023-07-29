78f6c1a3fb0446ba12d53cd0c3aaab29aa7c1f6a
andersentess <andersentess@gmail.com>
2023-07-28 17:36:51 -0700

    Created forecast template in JS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,34 @@ minutes = minutes < 10 ? "0" + minutes : minutes;
 let dateElement = document.querySelector("#date");
 dateElement.textContent = `${day} ${hours}:${minutes} ${amPm}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="https://openweathermap.org/img/wn/01d@2x.png"
+                alt=""
+                width="64"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max"> 18° </span>
+                <span class="weather-forecast-temperature-min"> 12° </span>
+              </div>
+            </div>
+          `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherSearch(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -91,3 +119,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("sydney");
+displayForecast();
